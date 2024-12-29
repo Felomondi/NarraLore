093dd7bda99e73595aa940747aeea09420639dcd
Felix Omondi <114206850+Felomondi@users.noreply.github.com>
2024-12-28 21:01:39 -0500

    Update db.py

--- backend/utils/db.py
+++ backend/utils/db.py
@@ -4,7 +4,7 @@ def get_db_connection():
     connection = mysql.connector.connect(
         host="localhost",  # Hostname (update if your DB is remote)
         user="root",  # Replace with your MySQL username
-        password="Kheed01@Vassar.edu",  # Replace with your MySQL password
+        password="Your DB password goes here",  # Replace with your MySQL password
         database="bookCritique_db"  # Your database name
     )
     return connection
