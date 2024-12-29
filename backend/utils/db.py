81504ce296e594a5ef469b5bbf65252d2919f91e
Felix Omondi <114206850+Felomondi@users.noreply.github.com>
2024-12-29 18:31:24 -0500

    Resolve the db

--- backend/utils/db.py
+++ backend/utils/db.py
@@ -8,11 +8,7 @@ def get_db_connection():
     connection = mysql.connector.connect(
         host="localhost",  # Hostname (update if your DB is remote)
         user="root",  # Replace with your MySQL username
-<<<<<<< HEAD
-        password="Your DB password goes here",  # Replace with your MySQL password
-=======
         password= os.getenv("DATABASE_PASSWORD"),  # Replace with your MySQL password
->>>>>>> cfbf52a (add bookdetails)
         database="bookCritique_db"  # Your database name
     )
     return connection
