7c817b764feeb4415137234d0e3c9c3b30caf9f4
felomondi <fomondi@vassar.edu>
2025-01-18 00:59:08 -0500

    add front page banner

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
-import logo from "./logo.png"; // Import the logo
+import logo from "../assets/logo.png"; // Import the logo
 
 const Navbar = ({ user, toggleSidebar, isSmallScreen }) => {
   return (
