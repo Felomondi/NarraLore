b8a95f7d2c9a2d56dc7d4197db473272c129b90c
felomondi <fomondi@vassar.edu>
2025-01-26 17:17:39 -0500

    cleanup

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,6 @@ from flask import Flask
 from flask_cors import CORS
 # Remove: from flask_jwt_extended import JWTManager
 from routes.book_routes import book_routes
-from routes.review_routes import review_routes
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
@@ -14,4 +13,3 @@ CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 # Remove auth routes registration
 app.register_blueprint(book_routes)
-app.register_blueprint(review_routes)
