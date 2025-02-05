7d30ddd90ca99f47cc40f19cb56fe5853e57e4c4
felomondi <fomondi@vassar.edu>
2025-02-05 00:56:25 -0500

    adding caching of books

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import axios from "axios";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Set Axios default base URL
 axios.defaults.baseURL = "http://127.0.0.1:5000";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// Ensure that your public/index.html contains <div id="root"></div>
+
+const queryClient = new QueryClient();
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
